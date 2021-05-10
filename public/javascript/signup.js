e2c3d2d6b6ea7948064ad66d3d23619f69361919
unknown <archernich@gmail.com>
2021-05-09 23:13:03 -0700

    signup bugfix

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -2,9 +2,9 @@ async function signupFormHandler(event) {
 
     event.preventDefault();
 
-    const username = document.querySelector('#signup-username');
-    const email = document.querySelector('#signup-email');
-    const password = document.querySelector('#signup-password');
+    const username = document.querySelector('#signup-username').value.trim();
+    const email = document.querySelector('#signup-email').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
