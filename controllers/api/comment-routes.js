ada137ee6c8e5cf81f00213ba5d6e22e9539997c
unknown <archernich@gmail.com>
2021-05-09 20:43:52 -0700

    single page and comment functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,10 +13,10 @@ router.get('/', (req, res) => {
 
 // CREATE A COMMENT
 router.post('/', (req, res) => {
-    // if (req.session) {
+    if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
-            user_id: req.body.user_id,
+            user_id: req.session.user_id,
             post_id: req.body.post_id
         })
             .then(dbCommentData => {res.json(dbCommentData)})
@@ -24,7 +24,7 @@ router.post('/', (req, res) => {
                 console.log(err);
                 res.status(400).json(err);
             });
-    //}
+    }
 });
 
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -139,7 +139,53 @@ router.get('/home', (req, res) => {
                 res.status(500).json(err);
             });
     };
-})
+});
+
+// SINGLE POST PAGE
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found here!' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('singlepost', { 
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        });
+});
+
+
+// NEW POST PAGE
+
 
 
 module.exports = router;
